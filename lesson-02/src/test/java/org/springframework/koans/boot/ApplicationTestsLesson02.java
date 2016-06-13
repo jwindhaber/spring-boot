faf90495afd4d58f5778e3ec19a1cb6aba9d8e77
jwindhaber <juergen.windhaber@gmail.com>
2016-06-13 17:39:07 +0200

    renamings

--- lesson-02/src/test/java/org/springframework/koans/boot/ApplicationTestsLesson02.java
+++ lesson-02/src/test/java/org/springframework/koans/boot/ApplicationTestsLesson02.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ApplicationLesson01.class)
-public class ApplicationTests {
+@SpringBootTest(classes = ApplicationLesson02.class)
+public class ApplicationTestsLesson02 {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
