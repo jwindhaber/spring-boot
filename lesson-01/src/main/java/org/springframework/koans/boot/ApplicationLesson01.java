8392220bdd7355bcf76bb9499d3a96848d9f0279
jwindhaber <juergen.windhaber@gmail.com>
2016-06-13 17:15:08 +0200

    Initial commit

---
+++ lesson-01/src/main/java/org/springframework/koans/boot/ApplicationLesson01.java
@@ -0,0 +1,12 @@
+package org.springframework.koans.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLesson01 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLesson01.class, args);
+	}
+}
