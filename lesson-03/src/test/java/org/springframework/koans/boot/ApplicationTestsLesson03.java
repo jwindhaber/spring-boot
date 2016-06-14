23eb45e9737c78e25216c2d4fc2dd591dd2c52b2
jwindhaber <juergen.windhaber@gmail.com>
2016-06-14 16:06:00 +0200

    Added negative usecase

--- lesson-03/src/test/java/org/springframework/koans/boot/ApplicationTestsLesson03.java
+++ lesson-03/src/test/java/org/springframework/koans/boot/ApplicationTestsLesson03.java
@@ -1,10 +1,13 @@
 package org.springframework.koans.boot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.koans.boot.controller.HelloController;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,12 +25,19 @@ public class ApplicationTestsLesson03 {
 	@Autowired
 	private MockMvc mvc;
 
+    @Autowired(required = false)
+    private JdbcTemplate jdbcTemplate;
+
 
 	@Test
 	public void testHelloWorld() throws Exception {
 
+        // Guard to check if persistence is not up!
+        Assert.assertNull(jdbcTemplate);
+
 		this.mvc.perform(get("/hello").accept(MediaType.TEXT_PLAIN))
 				.andExpect(status().isOk()).andExpect(content().string("Hello World!"));
+
 
 	}
 
